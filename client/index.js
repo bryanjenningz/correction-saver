ab9ed7c80136c7bbc0fee6e27b5cacd9db8421e7
Bryan Jennings <bryanjenningz@gmail.com>
2016-11-14 09:37:02 -0800

    add socket.io

--- client/index.js
+++ client/index.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
 import {render} from 'react-dom'
 
+const socket = io()
+
 class App extends Component {
   render() {
     return (
--- server/index.js
+++ server/index.js
@@ -1,4 +1,16 @@
 const express = require('express')
 const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+
 app.use(express.static('dist'))
-app.listen(8000, () => console.log('Listening on port 8000'))
+
+io.on('connection', (socket) => {
+  console.log('user connected')
+  
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+})
+
+server.listen(8000, () => console.log('Listening on port 8000'))
