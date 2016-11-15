8e17f1570da899f8041e45e393cf37f4f764ef7b
Bryan Jennings <bryanjenningz@gmail.com>
2016-11-15 10:26:13 -0800

    turn People component into a pure arrow function

--- client/components/People.js
+++ client/components/People.js
@@ -1,31 +1,22 @@
 import React, {Component, PropTypes} from 'react'
 
-export default class People extends Component {
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    return (
-      <div className="col-sm-offset-4 col-sm-4">
-        {this.props.people.map((person, i) =>
-          <a key={i} href={`#/chat/${i}`}>
-            <div className="row">
-              <div className="col-xs-3">
-                <img className="thumbnail" src="default.png" style={{width: 50, height: 50}} />
-              </div>
-              <div className="col-xs-9">
-                <h5 className="col-xs-4">Name: {person.name}</h5>
-                <h5 className="col-xs-4">Speaks: {person.speaks}</h5>
-                <h5 className="col-xs-4">Learning: {person.learning}</h5>
-              </div>
-            </div>
-          </a>
-        )}
-      </div>
-    )
-  }
-}
+const People = ({people}) =>
+  <div className="col-sm-offset-4 col-sm-4">
+    {people.map((person, i) =>
+      <a key={i} href={`#/chat/${i}`}>
+        <div className="row">
+          <div className="col-xs-3">
+            <img className="thumbnail" src="default.png" style={{width: 50, height: 50}} />
+          </div>
+          <div className="col-xs-9">
+            <h5 className="col-xs-4">Name: {person.name}</h5>
+            <h5 className="col-xs-4">Speaks: {person.speaks}</h5>
+            <h5 className="col-xs-4">Learning: {person.learning}</h5>
+          </div>
+        </div>
+      </a>
+    )}
+  </div>
 
 People.propTypes = {
   people: PropTypes.arrayOf(PropTypes.shape({
@@ -34,3 +25,5 @@ People.propTypes = {
     learning: PropTypes.string.isRequired
   })).isRequired
 }
+
+export default People
